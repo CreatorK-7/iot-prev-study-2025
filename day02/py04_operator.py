836c71a0351a2fe96b5ca42148f18c47e83b793b
JYK <jykim9226@gmail.com>
2025-01-15 16:19:42 +0900

    2일차 일단 커밋

---
+++ day02/py04_operator.py
@@ -0,0 +1,29 @@
+# 연산자
+
+a = 10
+b = 23
+
+c = a + b # 덧셈
+print(c)
+
+c = a - b # 뺄셈
+print(c)
+
+c = a * b # 곱셈 *(astrex)사용
+print(c)
+
+c = b / a # 나눗셈 > 결과가 실수
+print(c)
+
+c = b // a # 나눈 결과가 정수(몫)
+print(c)
+
+c = b % a # 나눈 결과의 나머지
+print(c)
+
+c = a ** 3 # 승수(거듭제곱) power 계산
+print(c)
+
+c = b / 0 # 문제! 예외 발생
+print(c)
+
